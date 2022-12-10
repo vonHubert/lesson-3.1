35be708461060cf37806ef2de34b9b7870b51e44
Sergey Sokolov <sergey.and.sokolov@gmail.com>
2022-12-10 04:19:47 +0400

    Homework 3.1

--- lesson 3.1/ViewController.swift
+++ lesson 3.1/ViewController.swift
@@ -10,39 +10,110 @@ import SpringAnimation
 
 class ViewController: UIViewController {
 
-    @IBOutlet var coreAnimationView: UIView!
-    @IBOutlet var springAnimationView: SpringView!
+    @IBOutlet var animationView: SpringView!
+    @IBOutlet var startAnimationButton: SpringButton!
+    @IBOutlet var currentAnimationDescriptionLabel: UILabel!
     
-    private var animationStarted = false
+    let animationPresets = [
+        "pop",
+        "slideLeft",
+        "slideRight",
+        "slideDown",
+        "slideUp",
+        "squeezeLeft",
+        "squeezeRight",
+        "squeezeDownv",
+        "squeezeUp",
+        "fadeIn",
+        "fadeOut",
+        "fadeOutIn",
+        "fadeInLeft",
+        "fadeInRight",
+        "fadeInDown",
+        "fadeInUp",
+        "zoomIn",
+        "zoomOut",
+        "fall",
+        "shake",
+        "flipX",
+        "flipY",
+        "morph",
+        "squeeze",
+        "flash",
+        "wobble",
+        "swing"
+    ]
+    
+    let animationCurves = [
+        "easeIn",
+        "easeOut",
+        "easeInOut",
+        "linear",
+        "spring",
+        "easeInSine",
+        "easeOutSine",
+        "easeInOutSine",
+        "easeInQuad",
+        "easeOutQuad",
+        "easeInOutQuad",
+        "easeInCubic",
+        "easeOutCubic",
+        "easeInOutCubic",
+        "easeInQuart",
+        "easeOutQuart",
+        "easeInOutQuart",
+        "easeInQuint",
+        "easeOutQuint",
+        "easeInOutQuint",
+        "easeInExpo",
+        "easeOutExpo",
+        "easeInOutExpo",
+        "easeInCirc",
+        "easeOutCirc",
+        "easeInOutCirc",
+        "easeInBack",
+        "easeOutBack",
+        "easeInOutBack"
+    ]
 
-    @IBAction func startCoreAnimation(_ sender: UIButton) {
-        sender.pulsate()
+    var nextAnimation = "pop"
+    
+    @IBAction func startAnimation(_ sender: SpringButton) {
+    
+        animationView.animation = nextAnimation
+        animationView.curve = animationCurves.randomElement()!
+        animationView.force = CGFloat.random(in: 1...10)
+        animationView.duration  = CGFloat.random(in: 1...10)
+        animationView.delay  = CGFloat.random(in: 0...1)
+        animationView.damping = CGFloat.random(in: 1...10)
+        animationView.velocity = CGFloat.random(in: 1...10)
+        animationView.repeatCount = Float.random(in: 1...10)
+        animationView.x = CGFloat.random(in: 1...100)
+        animationView.y = CGFloat.random(in: 1...100)
+        animationView.scaleX = CGFloat.random(in: 1...5)
+        animationView.scaleY = CGFloat.random(in: 1...5)
+        animationView.rotate = CGFloat.random(in: 1...10)
         
-        UIView.animate( // анимация движения для view
-            withDuration: 0.5, // длительность
-            delay: 0, // задержка старта в сек
-            options: [.autoreverse, .repeat]) {// доп опции (туда-обратно и повтор)
-                [unowned self] in   //лист захвата (лекция 2.14)
-                if !animationStarted { // для повтора
-                    coreAnimationView.frame.origin.x -= 40
-                    animationStarted.toggle()
-                }
-            }
+        currentAnimationDescriptionLabel.text = """
+    title: \(animationView.animation)
+    curve: \(animationView.curve)
+    force: \(String(format: "%.1f", animationView.force))
+    delay: \(String(format: "%.1f", animationView.duration))
+    duration: \(String(format: "%.1f", animationView.delay))
+    damping: \(String(format: "%.1f", animationView.damping))
+    velocity: \(String(format: "%.1f", animationView.velocity))
+    repeatCount: \(String(format: "%.1f", animationView.repeatCount))
+    x: \(String(format: "%.1f", animationView.x))
+    y: \(String(format: "%.1f", animationView.y))
+    scaleX: \(String(format: "%.1f", animationView.scaleX))
+    scaleY: \(String(format: "%.1f", animationView.scaleY))
+    rotate: \(String(format: "%.1f", animationView.rotate))
+"""
+        animationView.animate()
+        nextAnimation = animationPresets.randomElement()!
+        startAnimationButton.setTitle("Next: \(String(nextAnimation))", for: .normal)
     }
-    
-    @IBAction func startSpringAnimation(_ sender: SpringButton) {
-        springAnimationView.animation = "wobble"
-        springAnimationView.curve = "easeIn"
-        springAnimationView.force = 2
-        springAnimationView.duration = 1
-        springAnimationView.delay = 0.3
-        springAnimationView.animate()
-        
-        sender.animation = "swing"
-        sender.animate()
-         
-    }
-    
-    
 }
 
+
+    
